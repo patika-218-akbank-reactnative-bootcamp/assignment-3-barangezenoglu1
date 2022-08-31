7931ee2257df71d86e80d81d7c9e5ca7966e8cb2
Baran Gezenoğlu <86961373+barangezenoglu1@users.noreply.github.com>
2022-08-31 16:20:21 +0300

    Revert "Feature/home header"

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { View } from "react-native";
+import { StatusBar, View } from "react-native";
 import { UserProvider } from "./src/context/UserProvider";
 import { LoginScreen } from "./src/screens/LoginScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen } from "./src/screens/HomeScreen";
+import { Contacts } from "./src/screens/Contacts";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor={'#2AABEE'} />
       <UserProvider>
         <Stack.Navigator screenOptions={{headerShown: false}}>
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Contacts" component={Contacts} />
         </Stack.Navigator>
       </UserProvider>
     </NavigationContainer>
