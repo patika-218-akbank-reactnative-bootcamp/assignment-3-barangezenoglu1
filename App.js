19e49b6d0ceb84db399ee4c282d5cf0c17bb9cf4
Baran Gezenoğlu <86961373+barangezenoglu1@users.noreply.github.com>
2022-08-31 16:21:00 +0300

    Revert "Feature/home header"

--- App.js
+++ App.js
@@ -1,24 +1,21 @@
-import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
-import { StatusBar, View } from "react-native";
-import { UserProvider } from "./src/context/UserProvider";
-import { LoginScreen } from "./src/screens/LoginScreen";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { HomeScreen } from "./src/screens/HomeScreen";
-import { Contacts } from "./src/screens/Contacts";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-      <StatusBar backgroundColor={'#2AABEE'} />
-      <UserProvider>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Contacts" component={Contacts} />
-        </Stack.Navigator>
-      </UserProvider>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Hello world</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
